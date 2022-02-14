f35e8e3c4971e67ffce96cb6a0bfbda818680fa2
abouzaid <mohmmdabouzaid@gmail.com>
2022-02-14 10:39:54 +0200

    geocoding callback function

--- app.js
+++ app.js
@@ -1,22 +1,52 @@
-const request=require('postman-request')
-const url="http://api.weatherstack.com/current?access_key=cfaf0c2804129a32e2de34bad2aee021&query=New%20York&units=f"
+const request = require("postman-request");
+const geocoding=require('./utils/geocoging')
+/*
+const url =
+  "http://api.weatherstack.com/current?access_key=cfaf0c2804129a32e2de34bad2aee021&query=New%20York&units=f";
 
-const geocodeURL ='https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiYWJvdXphaWQxIiwiYSI6ImNremxueDRuaTE3YmcybmxsZnVzNnR0b2EifQ.imSdT_lql9jCx6oZQ9FDUQ&limit=1'
+const geocodeURL =
+  "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiYWJvdXphaWQxIiwiYSI6ImNremxueDRuaTE3YmcybmxsZnVzNnR0b2EifQ.imSdT_lql9jCx6oZQ9FDUQ&limit=1";
 /*requset({url:url,json:true},(error,response)=>{
   console.log(response.body.current.weather_descriptions[0]+'.It is cruelty '+ response.body.current.temperature+' degree out . There is a '+ response.body.current.feelslike+' % chance of rain')
   //console.log(response.body.current.temperature);
 
 })*/
-
+/*
 request({ url: geocodeURL, json: true }, (error, response) => {
+  if (error) {
+    console.log("Unable to connect to location services!");
+  } else if (response.body.features.length === 0) {
+    console.log("Unable to find location. Try another search.");
+  } else {
+    const latitude = response.body.features[0].center[0];
+    const longitude = response.body.features[0].center[1];
+    console.log(latitude, longitude);
+  }
+});
+*/
+/*
+const geocoding = (address,callback) => {
+  const url =
+    "https://api.mapbox.com/geocoding/v5/mapbox.places/" +
+    encodeURI( address)    +
+    ".json?access_token=pk.eyJ1IjoiYWJvdXphaWQxIiwiYSI6ImNremxueDRuaTE3YmcybmxsZnVzNnR0b2EifQ.imSdT_lql9jCx6oZQ9FDUQ&limit=1";
+
+  request({ url:url, json: true }, (error, response) => {
     if (error) {
-    console.log('Unable to connect to location services!')
+      callback("Unable to connect to location services!");
     } else if (response.body.features.length === 0) {
-    console.log('Unable to find location. Try another search.')
+      callback("Unable to find location. Try another search.");
     } else {
-    const latitude = response.body.features[0].center[0]
-    const longitude = response.body.features[0].center[1]
-    console.log(latitude, longitude)
+      const latitude = response.body.features[0].center[0];
+      const longitude = response.body.features[0].center[1];
+      const place=response.body.features[0].place_name;
+      callback(undefined,{latitude, longitude,place});
     }
-    
-    })
+  });
+};
+*/
+geocoding("ismailia", (error, data) => {
+  console.log("error", error);
+  console.log("data", data);
+});
+encodeURI
---
+++ utils/geocoging.js
@@ -0,0 +1,22 @@
+const request = require("postman-request");
+const geocoding = (address,callback) => {
+    const url =
+      "https://api.mapbox.com/geocoding/v5/mapbox.places/" +
+      encodeURI( address)    +
+      ".json?access_token=pk.eyJ1IjoiYWJvdXphaWQxIiwiYSI6ImNremxueDRuaTE3YmcybmxsZnVzNnR0b2EifQ.imSdT_lql9jCx6oZQ9FDUQ&limit=1";
+  
+    request({ url:url, json: true }, (error, response) => {
+      if (error) {
+        callback("Unable to connect to location services!");
+      } else if (response.body.features.length === 0) {
+        callback("Unable to find location. Try another search.");
+      } else {
+        const latitude = response.body.features[0].center[0];
+        const longitude = response.body.features[0].center[1];
+        const place=response.body.features[0].place_name;
+        callback(undefined,{latitude, longitude,place});
+      }
+    });
+  };
+
+  module.exports= geocoding
